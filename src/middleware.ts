a6a2633cd63803b388a34844452061f494cb1f56
muhammad.sukriyatma <muhammad.sukriyatma@bankneo.co.id>
2024-09-16 20:09:14 +0700

    add: handling session on cookies if secure connection

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
-    const cookie = request.cookies.has("next-auth.session-token")
+    const cookie = request.cookies.has("next-auth.session-token") || request.cookies.has("__Secure-next-auth.session-token")
     
     if (!cookie && pathname !== '/auth/login') {
         return NextResponse.redirect(new URL("/auth/login", request.url));
